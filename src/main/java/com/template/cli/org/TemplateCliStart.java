41a7f5496f1e76639128d7b25d60f430a364190f
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-09-10 22:45:36 -0300

    refactor: alterando nome da classe

---
+++ src/main/java/com/template/cli/org/TemplateCliStart.java
@@ -0,0 +1,20 @@
+package com.template.cli.org;
+
+import com.template.cli.org.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class TemplateCliStart {
+    public static void main(String[] args) {
+        try {
+            final ConfigurableApplicationContext context = SpringApplication.run(WebServerConfig.class, args);
+            final var exitCode = SpringApplication.exit(context);
+            System.exit(exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "local");
+    }
+}
