9a728d309fbe439a273506980a836cfa10815298
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-09-10 22:47:07 -0300

    feat: classe responsavel por informar ao Spring a estrutura de pasta para leitura dos arquivos e resolver dependencias

---
+++ src/main/java/com/template/cli/org/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.template.cli.org.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.template.cli.org")
+public class WebServerConfig {
+}
