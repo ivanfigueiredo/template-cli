3e99d2dea22a9cd0875532cdd7f0e0fe50bbfd85
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-09-11 23:10:54 -0300

    feat: adicionando dependencias

--- src/main/java/com/template/cli/org/infra/MainController.java
+++ src/main/java/com/template/cli/org/infra/MainController.java
@@ -1,7 +1,9 @@
 package com.template.cli.org.infra;
 
+import com.template.cli.org.application.creategitignore.CreateGitIgnore;
+import com.template.cli.org.application.createpom.CreatePom;
 import com.template.cli.org.application.directorystructure.DirectoryStructure;
-import com.template.cli.org.application.src.CreateMain;
+import com.template.cli.org.application.createmain.CreateMain;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import picocli.CommandLine;
@@ -12,19 +14,31 @@ import java.util.Objects;
 public class MainController implements CommandLineRunner {
 
     private final DirectoryStructure directoryStructure;
-
     private final CreateMain createMain;
+    private final CreateGitIgnore createGitIgnore;
+    private final CreatePom createPom;
 
-    public MainController(final DirectoryStructure directoryStructure, final CreateMain createMain) {
+    public MainController(
+            final DirectoryStructure directoryStructure,
+            final CreateMain createMain,
+            final CreateGitIgnore createGitIgnore,
+            final CreatePom createPom
+    ) {
         this.directoryStructure = Objects.requireNonNull(directoryStructure);
         this.createMain = Objects.requireNonNull(createMain);
+        this.createGitIgnore = Objects.requireNonNull(createGitIgnore);
+        this.createPom = Objects.requireNonNull(createPom);
     }
 
     @Override
     public void run(String... args) throws Exception {
         final CommandLine directoryStructureCommandLine = new CommandLine(directoryStructure);
         final CommandLine createMainCommandLine = new CommandLine(createMain);
+        final CommandLine createGitIgnoreCommandLine = new CommandLine(createGitIgnore);
+        final CommandLine createPomCommandLine = new CommandLine(createPom);
         directoryStructureCommandLine.execute(args);
         createMainCommandLine.execute(args);
+        createGitIgnoreCommandLine.execute(args);
+        createPomCommandLine.execute(args);
     }
 }
